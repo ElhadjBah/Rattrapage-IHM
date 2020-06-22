3f079f5b7d4b54c43563ab59b5d8fa7cb0e7c058
ElhadjBah <korka8492@gmail.com>
2020-06-23 01:01:53 +0200

    Ajout de la partie 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,10 @@ export class AppComponent  {
   grid: number [][];
   size: number;
   gridClass: string;
+  selectedButtonIndex: number;
 
   ngOnInit(){
+    /* this.sudokuService.generateBoard(); */
     this.sudokuService.boardObs.subscribe((result)=> {
       this.grid = result.grid;
       this.size = result.size;
@@ -27,7 +29,26 @@ export class AppComponent  {
         this.gridClass = 's4'
       }
     });
-    this.sudokuService.play(0, 1, 1);
+    this.sudokuService.play(0, 1, 7);
   }
 
+  //Bouton de génération de plateau
+  generateBoard() {
+    this.sudokuService.generateBoard();
+  }
+
+  //Lorsqu'on choisi un chiffre
+  onOptionSelected(index: number){
+    this.selectedButtonIndex = index;
+  }
+
+  //Lorsqu'on clique sur "Effacer"
+  onDeleteSelected(){
+    this.selectedButtonIndex = null;
+  }
+
+  isPlayableCell(i: number, j: number, v: number): boolean{
+    console.log(this.sudokuService.canPlay(i,j,v),v);
+    return this.sudokuService.canPlay(i,j,v);
+  }
 }
--- src/app/sudoku.service.ts
+++ src/app/sudoku.service.ts
@@ -1,17 +1,17 @@
-import {Injectable} from '@angular/core';
-import {getRandomConfig, CONFIG, COORD} from './generator';
-import {BehaviorSubject, Observable} from 'rxjs';
+import { Injectable } from "@angular/core";
+import { getRandomConfig, CONFIG, COORD } from "./generator";
+import { BehaviorSubject, Observable } from "rxjs";
 
 export interface BOARD {
-  size: number;     // Taille de la grille
+  size: number; // Taille de la grille
   grid: number[][]; // Une matrice size x size. La valeur 0 indique que la case n'a pas encore été remplie
 }
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class SudokuService {
-  private boardSubj = new BehaviorSubject<BOARD>( {
+  private boardSubj = new BehaviorSubject<BOARD>({
     size: 9,
     grid: [
       [3, 0, 6, 5, 0, 8, 4, 0, 0],
@@ -23,11 +23,12 @@ export class SudokuService {
       [1, 3, 0, 0, 0, 0, 2, 5, 0],
       [0, 0, 0, 0, 0, 0, 0, 7, 4],
       [0, 0, 5, 2, 0, 6, 3, 0, 0]
-      ]
-  } );
+    ]
+  });
   readonly boardObs = this.boardSubj.asObservable();
 
   constructor() {
+    this.generateBoard();
   }
 
   /* generateBoard génère un nouveau plateau et le publie via bordSubj
@@ -36,6 +37,12 @@ export class SudokuService {
    */
   async generateBoard() {
     // a completer
+    const PConfig: CONFIG = await getRandomConfig();
+    const builtBoard: BOARD = {
+      size: Number.parseInt(PConfig.size),
+      grid: this.buildGrid(PConfig)
+    };
+    this.boardSubj.next(builtBoard);
   }
 
   // canPlay renvoie vrai si, dans le plateau B, il est possible d'inscrire la valeur v
@@ -46,16 +53,16 @@ export class SudokuService {
     let result = true;
     let size = this.boardSubj.value.size;
     //on commence par verifier s'il n'est pas présent sur la ligne i
-    for(let index_column = 0; index_column < size && result; index_column++) {
-      if (this.boardSubj.value.grid[i][index_column]== v) {
+    for (let index_column = 0; index_column < size && result; index_column++) {
+      if (this.boardSubj.value.grid[i][index_column] == v) {
         result = false;
       }
     }
 
     //Si on ne trouve pas v sur la ligne i, on verifie s'il n'est pas présent sur la colonne j
-    if (result){
-      for(let index_line = 0; index_line < size && result; index_line++) {
-        if (this.boardSubj.value.grid[index_line][j]== v) {
+    if (result) {
+      for (let index_line = 0; index_line < size && result; index_line++) {
+        if (this.boardSubj.value.grid[index_line][j] == v) {
           result = false;
         }
       }
@@ -63,112 +70,156 @@ export class SudokuService {
 
     //Si v n'est ni dans i, ni dans j, on verifie s n'il n'est pas present dans le bloc contenant la case (i,j)
     // pour les grilles de taille 9
-    if (result){
-      if (size == 9){
-        if (i>=0 && i <= 2){
-          for(let index_line = 0; index_line < 3 && result; index_line++) {
-            if (j>=0 && j<=2) {
-              for(let index_column = 0; index_column < 3 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+    if (result) {
+      if (size == 9) {
+        if (i >= 0 && i <= 2) {
+          for (let index_line = 0; index_line < 3 && result; index_line++) {
+            if (j >= 0 && j <= 2) {
+              for (
+                let index_column = 0;
+                index_column < 3 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[index_line][index_column] == v) {
                   result = false;
                 }
               }
-            }
-            else if (j>=3 && j<=5) {
-              for(let index_column = 3; index_column < 6 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+            } else if (j >= 3 && j <= 5) {
+              for (
+                let index_column = 3;
+                index_column < 6 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[index_line][index_column] == v) {
                   result = false;
                 }
               }
-            }
-            else if (j>=6 && j<=8) {
-              for(let index_column = 6; index_column < 9 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
-                  result = false;
-                }
-              }
-            }
-          }
-        }else if (i>=3 && i <= 5){
-          for(let index_line = 3; index_line < 6 && result; index_line++) {
-            if (j>=0 && j<=2) {
-              for(let index_column = 0; index_column < 3 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
-                  result = false;
-                }
-              }
-            }
-            else if (j>=3 && j<=5) {
-              for(let index_column = 3; index_column < 6 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
-                  result = false;
-                }
-              }
-            }
-            else if (j>=6 && j<=8) {
-              for(let index_column = 6; index_column < 9 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+            } else if (j >= 6 && j <= 8) {
+              for (
+                let index_column = 6;
+                index_column < 9 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[index_line][index_column] == v) {
                   result = false;
                 }
               }
             }
           }
-        }else if (i>=6 && i <= 8){
-          for(let index_line = 6; index_line < 9 && result; index_line++) {
-            if (j>=0 && j<=2) {
-              for(let index_column = 0; index_column < 3 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+        } else if (i >= 3 && i <= 5) {
+          for (let index_line = 3; index_line < 6 && result; index_line++) {
+            if (j >= 0 && j <= 2) {
+              for (
+                let index_column = 0;
+                index_column < 3 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
-            }
-            else if (j>=3 && j<=5) {
-              for(let index_column = 3; index_column < 6 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+            } else if (j >= 3 && j <= 5) {
+              for (
+                let index_column = 3;
+                index_column < 6 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
-            }
-            else if (j>=6 && j<=8) {
-              for(let index_column = 6; index_column < 9 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+            } else if (j >= 6 && j <= 8) {
+              for (
+                let index_column = 6;
+                index_column < 9 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
             }
           }
-        }  
+        } else if (i >= 6 && i <= 8) {
+          for (let index_line = 6; index_line < 9 && result; index_line++) {
+            if (j >= 0 && j <= 2) {
+              for (
+                let index_column = 0;
+                index_column < 3 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
+                  result = false;
+                }
+              }
+            } else if (j >= 3 && j <= 5) {
+              for (
+                let index_column = 3;
+                index_column < 6 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
+                  result = false;
+                }
+              }
+            } else if (j >= 6 && j <= 8) {
+              for (
+                let index_column = 6;
+                index_column < 9 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
+                  result = false;
+                }
+              }
+            }
+          }
+        }
       } else if (size == 4) {
         // pour les grilles de taille 4
-        if (i>=0 && i <= 1){
-          for(let index_line = 0; index_line < 2 && result; index_line++) {
-            if (j>=0 && j<=1) {
-              for(let index_column = 0; index_column < 2 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+        if (i >= 0 && i <= 1) {
+          for (let index_line = 0; index_line < 2 && result; index_line++) {
+            if (j >= 0 && j <= 1) {
+              for (
+                let index_column = 0;
+                index_column < 2 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
-            }
-            else if (j>=2 && j<=3) {
-              for(let index_column = 2; index_column < 4 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+            } else if (j >= 2 && j <= 3) {
+              for (
+                let index_column = 2;
+                index_column < 4 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
             }
           }
-        }else if (i>=2 && i <= 3){
-          for(let index_line = 2; index_line < 4 && result; index_line++) {
-            if (j>=0 && j<=1) {
-              for(let index_column = 0; index_column < 2 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+        } else if (i >= 2 && i <= 3) {
+          for (let index_line = 2; index_line < 4 && result; index_line++) {
+            if (j >= 0 && j <= 1) {
+              for (
+                let index_column = 0;
+                index_column < 2 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
-            }
-            else if (j>=2 && j<=3) {
-              for(let index_column = 2; index_column < 4 && result; index_column++) {
-                if (this.boardSubj.value.grid[i][j]== v) {
+            } else if (j >= 2 && j <= 3) {
+              for (
+                let index_column = 2;
+                index_column < 4 && result;
+                index_column++
+              ) {
+                if (this.boardSubj.value.grid[i][j] == v) {
                   result = false;
                 }
               }
@@ -177,17 +228,37 @@ export class SudokuService {
         }
       }
     }
-    
+
     return result;
   }
 
   // play(i, j, v) inscrit la valeur v dans la case (i, j) si canplay(i, j, v)
   // play publie un nouveau plateau dans ce cas via la méthode next de l'attribut boardSubj
   play(i: number, j: number, nv: number) {
-    if (this.canPlay(i,j,nv)) {
+    if (this.canPlay(i, j, nv)) {
       this.boardSubj.value.grid[i][j] = nv;
       this.boardSubj.next(this.boardSubj.value);
     }
   }
 
+  // Construction du grid à partir de la config obtenue
+  buildGrid(config: CONFIG): number[][] {
+    let size = Number.parseInt(config.size);
+    let builtGrid: number[][] = this.initVoidGrid([size, size]);
+    config.squares.map(element => {
+      builtGrid[element.x][element.y] = element.value;
+    });
+    return builtGrid;
+  }
+
+  // Initialisation d'un tableau rempli de 0
+  initVoidGrid(dimensions: number[]): number[][] {
+    const array = [];
+    for (let i = 0; i < dimensions[0]; ++i) {
+      array.push(
+        dimensions.length == 1 ? 0 : this.initVoidGrid(dimensions.slice(1))
+      );
+    }
+    return array;
+  }
 }
